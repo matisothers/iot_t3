1bbebf6a1d30c38949c18fef19f504a740378a6a
Mrtn-fa <mnfaray@gmail.com>
2023-12-05 22:01:58 -0300

    env variables added

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+const BACKEND_URL = process.env.BACKEND_URL;
+const BACKEND_PORT = process.env.BACKEND_PORT;
 
-let backendUrl = "http://localhost:8000"; // Esto lo deben de modificar (idealmente una variable de entorno)
+let backendUrl = `http://${BACKEND_URL}:${BACKEND_PORT}`; // Esto lo deben de modificar (idealmente una variable de entorno)
 
 if (backendUrl && backendUrl.startsWith('"') && backendUrl.endsWith('"')) {
   backendUrl = backendUrl.slice(1, -1);
